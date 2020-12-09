b33589f209992db9880e1467f83aa3cc558939ff
Thiago Vieira <th91vi@gmail.com>
2020-12-09 12:50:59 -0300

    feat: added quantity selector, add to cart button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header"
 import Footer from "./components/Footer"
 import HomeScreen from "./pages/HomeScreen"
 import ProductScreen from "./pages/ProductScreen"
+import CartScreen from "./pages/CartScreen"
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Container>
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
