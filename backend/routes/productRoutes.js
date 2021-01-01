7093ebeb64461bb9bafe7aedf9b8562f75442722
Thiago Vieira <th91vi@gmail.com>
2021-01-01 17:52:46 -0300

    refactor: product routing is now done with a controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,8 @@
 const express = require('express');
-const asyncHandler = require('express-async-handler');
 const router = express.Router();
-const Product = require('../models/productModel');
+const ProductController = require('../controllers/productController');
 
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    res.json(products);
-  })
-);
-
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(ProductController.getProducts);
+router.route('/:id').get(ProductController.getProductById);
 
 module.exports = router;
