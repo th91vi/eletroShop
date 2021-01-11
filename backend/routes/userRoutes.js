0a333791caca797bf76912331aaef96198841f77
Thiago Vieira <th91vi@gmail.com>
2021-01-11 12:40:07 -0300

    feat: custom authentication middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
+const AuthMiddleware = require('../middleware/authMiddleware');
 
-router.route('/login').get(UserController.authUser);
+router.route('/login').post(UserController.authUser);
+router
+  .route('/profile')
+  .get(AuthMiddleware.protect, UserController.getUserProfile);
 
 module.exports = router;
