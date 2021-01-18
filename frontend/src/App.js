c8ef7e1c9784c9aca1e8c774d2bf2fc5276024fb
Thiago Vieira <th91vi@gmail.com>
2021-01-18 08:15:29 -0300

    feat: profile screen, get user details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from "./pages/ProductScreen"
 import CartScreen from "./pages/CartScreen"
 import LoginScreen from "./pages/LoginScreen"
 import RegisterScreen from "./pages/RegisterScreen"
+import ProfileScreen from "./pages/ProfileScreen"
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
+          <Route path="/profile" component={ProfileScreen} />
           <Route path="/login" component={LoginScreen} />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/product/:id" component={ProductScreen} />
