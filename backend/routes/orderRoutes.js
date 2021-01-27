787a6e059ccbec941c447f7cc4082bf0064c22f8
Thiago Vieira <th91vi@gmail.com>
2021-01-27 12:40:47 -0300

    feat: get order by id endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,5 +4,6 @@ const OrderController = require('../controllers/orderController');
 const AuthMiddleware = require('../middleware/authMiddleware');
 
 router.route('/').post(AuthMiddleware.protect, OrderController.addOrderItems);
+router.route('/:id').get(AuthMiddleware.protect, OrderController.getOrderById);
 
 module.exports = router;
