ab9349e83a95f9c7824c1d5c88f81a95e8dbb01a
Thiago Vieira <th91vi@gmail.com>
2021-02-19 14:53:20 -0300

    feat: update user actions, reducers

--- frontend/src/pages/UserEditScreen.js
+++ frontend/src/pages/UserEditScreen.js
@@ -6,7 +6,8 @@ import { useDispatch, useSelector } from "react-redux"
 import Message from "../components/Message"
 import Loader from "../components/Loader"
 import FormContainer from "../components/FormContainer"
-import { getUserDetails } from "../actions/userActions"
+import { getUserDetails, updateUser } from "../actions/userActions"
+import { USER_UPDATE_RESET } from "../constants/userConstants"
 
 const UserEditScreen = ({ match, history }) => {
   const userId = match.params.id
@@ -20,20 +21,42 @@ const UserEditScreen = ({ match, history }) => {
   const userDetails = useSelector((state) => state.userDetails)
   const { loading, error, user } = userDetails
 
-  console.log(getUserDetails, userId, dispatch, user, history)
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
+  const userUpdate = useSelector((state) => state.userUpdate)
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = userUpdate
 
   useEffect(() => {
-    if (!user.name || user._id !== userId) {
-      dispatch(getUserDetails(userId))
+    if (successUpdate) {
+      dispatch({ type: USER_UPDATE_RESET })
+      history.push("/admin/userlist")
     } else {
-      setName(user.name)
-      setEmail(user.email)
-      setIsAdmin(user.isAdmin)
+      if (!user.name || user._id !== userId) {
+        dispatch(getUserDetails(userId))
+      } else {
+        setName(user.name)
+        setEmail(user.email)
+        setIsAdmin(user.isAdmin)
+      }
     }
-  }, [user, dispatch, userId])
+  }, [
+    user,
+    dispatch,
+    userId,
+    successUpdate,
+    loadingUpdate,
+    errorUpdate,
+    history,
+  ])
 
   const submitHandler = (e) => {
     e.preventDefault()
+    dispatch(updateUser({ _id: userId, name, email, isAdmin }))
   }
 
   return (
@@ -43,6 +66,8 @@ const UserEditScreen = ({ match, history }) => {
       </Link>
       <FormContainer>
         <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
         {loading ? (
           <Loader />
         ) : error ? (
@@ -67,14 +92,16 @@ const UserEditScreen = ({ match, history }) => {
                 onChange={(e) => setEmail(e.target.value)}
               ></Form.Control>
             </Form.Group>
-            <Form.Group controlId="isAdmin">
-              <Form.Check
-                type="checkbox"
-                label="Is Admin"
-                checked={isAdmin}
-                onChange={(e) => setIsAdmin(e.target.checked)}
-              ></Form.Check>
-            </Form.Group>
+            {userInfo._id !== user._id && (
+              <Form.Group controlId="isAdmin">
+                <Form.Check
+                  type="checkbox"
+                  label="Is Admin"
+                  checked={isAdmin}
+                  onChange={(e) => setIsAdmin(e.target.checked)}
+                ></Form.Check>
+              </Form.Group>
+            )}
             <Button type="submit" variant="primary">
               Update
             </Button>
