339997c25178cf7e008b9b69bc3635c994f7db93
Thiago Vieira <th91vi@gmail.com>
2021-02-22 13:15:04 -0300

    feat: added deleteProducts route and controller method

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -11,11 +11,24 @@ const getProductById = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (product) {
-    res.json(product);
+    await product.remove();
+    res.json({ message: 'Product removed' });
   } else {
     res.status(404);
     throw new Error('Product not found');
   }
 });
 
-module.exports = { getProducts, getProductById };
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json('Product removed');
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+module.exports = { getProducts, getProductById, deleteProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const ProductController = require('../controllers/productController');
+const AuthMiddleware = require('../middleware/authMiddleware');
 
 router.route('/').get(ProductController.getProducts);
-router.route('/:id').get(ProductController.getProductById);
+router
+  .route('/:id')
+  .get(ProductController.getProductById)
+  .delete(AuthMiddleware.protect, ProductController.deleteProduct);
 
 module.exports = router;
